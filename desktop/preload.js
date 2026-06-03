9580b5fa45a6b23ec6d013d9065829f623084af3
souhailted2 <souhailted2@gmail.com>
2026-06-03 22:40:54 +0100

    sync: auto-sync bidirectional engine

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,9 +1,22 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  checkOnline: () => ipcRenderer.invoke("check-online"),
-  getVersion:  () => ipcRenderer.invoke("get-app-version"),
-  backupDb:    () => ipcRenderer.invoke("backup-db"),
-  restoreDb:   () => ipcRenderer.invoke("restore-db"),
+  checkOnline:         () => ipcRenderer.invoke("check-online"),
+  getVersion:          () => ipcRenderer.invoke("get-app-version"),
+  backupDb:            (destPath) => ipcRenderer.invoke("backup-db", destPath),
+  restoreDb:           (srcPath)  => ipcRenderer.invoke("restore-db", srcPath),
   isElectron: true,
+
+  // Auto-sync IPC
+  getSyncStatus:       ()                    => ipcRenderer.invoke("get-sync-status"),
+  saveSyncCredentials: ({ username, password }) => ipcRenderer.invoke("save-sync-credentials", { username, password }),
+  triggerSync:         ()                    => ipcRenderer.invoke("trigger-sync"),
+
+  // Listen for push sync-status events from main
+  onSyncStatus: (callback) => {
+    ipcRenderer.on("sync-status", (_event, status) => callback(status));
+  },
+  removeSyncStatusListener: () => {
+    ipcRenderer.removeAllListeners("sync-status");
+  },
 });
